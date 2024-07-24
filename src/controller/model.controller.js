d2fe5a5d02953e76c3b5f9a8a58a581c2bedec4b
subhajit60 <subhajitgarai988@gmail.com>
2024-07-24 08:45:52 +0530

    update netlify.toml

--- src/controller/model.controller.js
+++ src/controller/model.controller.js
@@ -3,9 +3,11 @@ import formdata from "form-data"
 import fs from "fs"
 import { uploadOnCloudinary } from "../middlewares/cloudinary.middleware.js"
 import { Prediction } from "../models/prediction.model.js"
+import errHandler from "../utils/errHandler.js"
 
-export const predictions = async (req, res) => {
+export const predictions = async (req, res, next) => {
 
+    //path selaction
     let localPath;
 
     if (req.user.role === 'admin') {
@@ -15,46 +17,84 @@ export const predictions = async (req, res) => {
     } else {
         localPath = 'uploads/others';
     }
-    if(!fs.readdirSync(localPath)){
+    if (!fs.readdirSync(localPath)) {
         fs.mkdirSync(localPath)
     }
-    let files = fs.readdirSync(localPath)
     
-    files.forEach(async (file) => {
-        let path = `${localPath}/${file}`
-        let imageData = fs.createReadStream(path);
-        let data = await imagepredictions(imageData)
-        //  upload to db
-        fileUpload(data, req, res, path)
-    });
+    //  reading file for images 
 
-    return res.json({ success: true, message: "file saved successfuly" })
+    let files = fs.readdirSync(localPath)
 
-}
+    let status = await processFiles(files,localPath,req);
 
-const imagepredictions = async (imageData) => {
-    const form = new formdata();
-    form.append("file", imageData);
-
-    const response = await axios.post(`${process.env.MODEL_URL}/predict`, form, {
-        headers: {
-            ...form.getHeaders()
+    if (status) {
+        if (status.every(Boolean)) {
+            res.status(200).json({ message: "All images processed" })
+        } else {
+            res.status(400).json({ message: "Some images not processed" })
         }
-    })
-    return response.data;
+    } else {
+        next(new errHandler("model not online ", "500"))
+    }
+
+
 }
 
-export const fileUpload = async (data, req, res, path) => {
+async function processFiles(files,localPath,req) {
+    const status = [];
+
+    for (const file of files) {
+        const path = `${localPath}/${file}`;
+        try {
+            const info = await imagepredictions(path);
+            const flag = await fileUpload(info, req, path);
+            if (flag) {
+                status.push(info || null);
+            }
+        } catch (error) {
+            console.error(`Error processing file ${file}:`, error);
+        }
+    }
+    if (!status.length > 0) {return null}
+    return status
+}
+
+
+const imagepredictions = async (path) => {
+    try {
+
+        let imageData = fs.createReadStream(path);
+        const form = new formdata();
+        form.append("file", imageData);
+
+        const response = await axios.post(`${process.env.MODEL_URL}/predict`, form, {
+            headers: {
+                ...form.getHeaders()
+            }
+        })
+        return response.data;
+    } catch (error) {
+        return null;
+    }
+}
+
+export const fileUpload = async (data, req, path) => {
+
     // file upload to cloudnary
+
     let cloudnaryUrl = await uploadOnCloudinary(path);
+    if (!data) { return null; }
+    
+    // store image and predictions in to mongo db
 
     if (cloudnaryUrl) {
-         try {
-            await Prediction.create({userid:req.user._id,prediction:{class:data.class,confidence:data.confidence},imageadd:cloudnaryUrl})
-            
-         } catch (error) {
-            console.log("Error in file upload to db --->",error);
-         }
+        try {
+            await Prediction.create({ userid: req.user._id, prediction: { class: data.class, confidence: data.confidence }, imageadd: cloudnaryUrl })
+            return 1;
+        } catch (error) {
+            console.log("Error in file upload to db --->", error);
+            return -1;
+        }
 
     }
 }
--- src/server.js
+++ src/server.js
@@ -1,13 +1,15 @@
 import { app } from "./App.js";
 import { Dbconnection } from "./utils/DB.js";
 import {errorMiddleware} from "./middlewares/error.middlewere.js"
+import axios from "axios";
 // 
 
 
 app.use(errorMiddleware);
 
 
-
+  
 Dbconnection().then(()=>{
     app.listen(process.env.PORT || 4000, console.log("surver listen on port ", process.env.PORT))
+    console.log("model server",process.env.MODEL_URL);
 })
